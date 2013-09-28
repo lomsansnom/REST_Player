ebe59d1a10dbdc65345c30b1c47efac59602484f
alex <alex@localhost.localdomain>
2013-09-28 23:04:37 +0200

    création de la fonction getListeMusiques (pas terminée)

---
+++ restPlayer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import cherrypy
+from cherrypy import expose
+import json
+import os
+
+class restPlayer:
+    
+    __contentType = "application/json;charset=utf-8"
+
+    def __init__(self):
+        cherrypy.response.headers['Content-Type'] = self.__contentType
+        
+    @expose
+    def getListeMusiques(self):
+        try:
+            params = json.loads(cherrypy.request.body.readline())
+        except Exception as e:
+            ret = {"OK" : False}
+            ret['Erreur'] = "Paramètres invalides"
+            cherrypy.log(str(e))
+            return json.dumps(ret)
+        
+        if "chemin" in params:
+            cherrypy.log(params["chemin"])
+            ret = os.listdir(params["chemin"])
+            cherrypy.log(str(ret))
+        else:
+            ret = {"OK" : False}
+            ret["Erreur"] = "chemin est obligatoire"
+        
+        return json.dumps(ret)
+        
+conf={  
+        'global':{
+                  'server.socket_host' : '0.0.0.0',
+                  'server.socket_port' : 8282,
+                  'log.screen' : True
+        },
+        '/' : {
+               'tools.encode.on':True,
+               'tools.encode.encoding':'utf-8'
+        }
+}
+
+
+cherrypy.config.update(conf)
+cherrypy.quickstart(restPlayer(),"/", conf)
