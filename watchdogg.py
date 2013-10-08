d417cbad507893ca6958bd71d5606b1eb986ec8b
alex <alex@localhost.localdomain>
2013-10-08 19:17:50 +0200

    test : ajout d'une pause de 10s après la détection d'un fichier pour être sûr que la copie soit terminée

--- watchdogg.py
+++ watchdogg.py
@@ -19,6 +19,7 @@ class MyEventHandler(FileSystemEventHandler):
         if len(os.listdir('/home/pi/Player')) == 1:
             if event.src_path[len(event.src_path)-4:] == ".mp3":
                 try:
+                    time.sleep(10)
                     print "Load musique : " + event.src_path
                     pygame.mixer.music.load(event.src_path)
                     print "play musique"
